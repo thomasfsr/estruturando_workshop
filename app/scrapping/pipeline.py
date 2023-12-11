d57acb1d284339fce4755a2ed8c8ab3ddb5b50f2
Thomás Freire <thomas.fsr@gmail.com>
2023-12-11 20:50:40 -0300

    funções do webscrapping e o pipeline

---
+++ app/scrapping/pipeline.py
@@ -0,0 +1,41 @@
+from scrapping.scrapping import scrape_data, \
+extract_table_data, \
+extract_ocorrencias, \
+replace_portuguese_months, \
+extract_and_convert_date, \
+get_tables, \
+create_df_type
+
+def pipeline(url):
+    tables, doc = scrape_data(url)
+    df = extract_table_data(tables)
+    ocorr = extract_ocorrencias(doc)
+    ocorrencia_time = pd.DataFrame(ocorr, columns=['ocorrencias'])
+    ocorrencias_br = pd.DataFrame(ocorr, columns=['ocorrencias'])
+    replace_portuguese_months(ocorrencia_time, "ocorrencias")
+    extract_and_convert_date(ocorrencia_time, 'ocorrencias' )
+    df_tabular = pd.concat([pd.concat([create_df_type(get_tables('capital','raw'),'capital'),ocorrencia_time['date']],axis=1),
+                        pd.concat([create_df_type(get_tables('demacro','raw'),'demacro'),ocorrencia_time['date']],axis=1),
+                        pd.concat([create_df_type(get_tables('interior','raw'),'interior'),ocorrencia_time['date']],axis=1)],
+                       axis=0)
+    df_tabular = df_tabular.sort_values('date').reset_index(drop=True)
+    df_tabular['feminicídio'].fillna(0, inplace=True)
+    df_tabular.to_csv('df_tabular.csv')
+
+    data_folder_path = os.path.join(os.pardir, 'data')
+    if not os.path.exists(data_folder_path):
+    # Create the "data" folder if it doesn't exist
+        os.makedirs(data_folder_path)
+    
+    csv_file_path = os.path.join(data_folder_path, 'df_tabular.csv')
+
+    # Save the DataFrame to a CSV file
+    df_tabular.to_csv(csv_file_path, index=False)
+
+    
+
+
+
+
+
+
---
+++ app/scrapping/scrapping.py
@@ -0,0 +1,116 @@
+# imports
+import pandas as pd
+import requests
+from selectolax import HTMLParser
+import datetime
+
+def scrape_data(url):
+    response = requests.get(url)
+    html = response.text
+    doc = HTMLParser(html)
+    tables = doc.css("table.table.table-striped.table-hover.table-condensed")
+    return tables, doc
+
+def extract_table_data(tables):
+    table_data = []
+
+    for table in tables:
+        for row in table.css("tr"):
+            row_data = []
+            cells = row.css("th, td") 
+            for cell in cells:
+                row_data.append(cell.text().strip())
+
+            if any(row_data):
+                table_data.append(row_data)
+
+    df = pd.DataFrame(table_data[1:], columns=table_data[0])
+    return df
+
+def extract_ocorrencias(doc):
+
+    ocorrencias = []
+
+    table_number = 2 
+    while True:
+        table_id = f"conteudo_repPeriodo_lblPeriodo_{table_number}"
+        span_element = doc.css(f'#{table_id}')
+
+        if not span_element:
+            break
+
+        ocorrencia = span_element[0].text()
+        ocorrencias.append(ocorrencia)
+
+        table_number += 1
+
+    return ocorrencias
+
+import datetime
+import pandas as pd
+
+def replace_portuguese_months(data_frame, column_name):
+    month_mapping = {
+        'Janeiro': 'January',
+        'Fevereiro': 'February',
+        'Março': 'March',
+        'Abril': 'April',
+        'Maio': 'May',
+        'Junho': 'June',
+        'Julho': 'July',
+        'Agosto': 'August',
+        'Setembro': 'September',
+        'Outubro': 'October',
+        'Novembro': 'November',
+        'Dezembro': 'December'
+    }
+
+    for pt_month, en_month in month_mapping.items():
+        data_frame[column_name] = data_frame[column_name].str.replace(pt_month, en_month)
+
+def extract_and_convert_date(data_frame, column_name):
+    data_frame['date'] = data_frame[column_name].apply(lambda x: datetime.datetime.strptime(x.split(': ')[-1], '%B de %Y'))
+
+def get_tables(df:str,choose:str):
+    ind = {'capital':1,'demacro':2,'interior':3,'total':4}
+
+    i = ind[df]
+    df1 = []
+    
+    for table in tables[:67]:
+        row_data = []
+        rows = table.css('tr')[1:]
+        for row in rows:
+            cell = row.css("td")[i]
+            row_data.append(cell.text().strip())
+        row_data = np.array(row_data).T
+        df1.append(row_data)
+        
+# Create a DataFrame from the extracted data
+    df_df1 = pd.DataFrame(df1, columns = col1).astype('uint8')
+    
+    df2 = []
+    
+    for table in tables[67:]:
+        row_data = []
+        rows = table.css('tr')[1:]
+        for row in rows:
+            cell = row.css("td")[i]
+            row_data.append(cell.text().strip())
+        row_data = np.array(row_data).T
+        df2.append(row_data)
+        
+# Create a DataFrame from the extracted data
+    df_df2 = pd.DataFrame(df2, columns = col2).astype('uint8')
+    
+    df = pd.concat([df_df1[col2],df_df2],axis=0, ignore_index=True)
+    dfraw = pd.concat([df_df1,df_df2],axis=0, ignore_index=True)
+    typee = {'new':df1, 'old':df2,'total':df,'raw':dfraw}
+    returned = typee[choose]
+    return returned
+
+
+
+def create_df_type(df:pd.DataFrame,name:str):
+    df = df.assign(regiao=name)
+    return df
